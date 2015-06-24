7da816244588641b561c8333d7370037da9b89b5
Gianlucca Puglia <gian0591@hotmail.com>
2015-06-24 17:08:29 -0300

    Criação do Commons para agregar itens em comum do Server e do Client

---
+++ Commons/src/com/dist/common/Operation.java
@@ -0,0 +1,24 @@
+package com.dist.common;
+
+public enum Operation {
+
+    RESERVAR("RESERVAR",1),
+    CONSULTAR("CONSULTAR",2);
+    
+    private String name;
+    private int code;
+    
+    private Operation(String name, int code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    
+}
